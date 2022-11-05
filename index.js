292d5f1ef03fd29f1fc001906dc42b1924d30981
Blackork1 <79012830+Blackork1@users.noreply.github.com>
2022-11-05 16:43:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ submit.addEventListener('submit', (e) => {
     `
     Email.send({
         SecureToken: "136d9e19-2795-46ff-a9bd-7f8b87ae110c",
-        To : 'pzaporozhchenko75@gmail.com',
+        To : 'blubberberndlp@googlemail.com',
         From : "blubberberndlp@googlemail.com",
         Subject : "Test Mail from" + email.value,
         Body : ebody
@@ -40,3 +40,43 @@ window.addEventListener("load", function() {
     var datetimeField = document.getElementById("date");
     datetimeField.value = formattedDate;
 });
+
+document.querySelector(".text-animation1").innerHTML =  
+document.querySelector(".text-animation1").textContent.replace(/./g, "<span>$&</span>");
+
+let spans1 = document.querySelectorAll(".text-animation1 span");
+for (let i = 0; i < spans1.length; i++)
+ {
+    let left = innerWidth * Math.random();
+    let top = innerHeight * Math.random();
+    if (Math.random() < 0.5) {
+        spans1[i].style.left = "-"+left+"px";
+    } else {
+        spans1[i].style.left = left+"px";
+    }
+    if (Math.random() < 0.5) {
+        spans1[i].style.top = "-"+top+"px";
+    } else {
+        spans1[i].style.top = top+"px";
+    }
+ }
+
+document.querySelector(".text-animation2").innerHTML =  
+document.querySelector(".text-animation2").textContent.replace(/./g, "<span>$&</span>");
+
+ let spans2 = document.querySelectorAll(".text-animation2 span");
+ for (let i = 0; i < spans2.length; i++)
+  {
+     let left = innerWidth * Math.random();
+     let top = innerHeight * Math.random();
+     if (Math.random() < 0.5) {
+        spans2[i].style.left = "-"+left+"px";
+     } else {
+        spans2[i].style.left = left+"px";
+     }
+     if (Math.random() < 0.5) {
+        spans2[i].style.top = "-"+top+"px";
+     } else {
+        spans2[i].style.top = top+"px";
+     }
+  }
